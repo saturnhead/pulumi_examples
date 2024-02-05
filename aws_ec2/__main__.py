ba5d4dd28799422158f8aa014ee6405a96899f86
Flavius <flaviuscdinu93@gmail.com>
2024-02-05 15:11:03 +0200

    Initial version

---
+++ aws_ec2/__main__.py
@@ -0,0 +1,18 @@
+import pulumi
+from pulumi_aws import ec2
+
+# Create a new security group
+security_group = ec2.SecurityGroup('my-security-group',
+    description='Enable SSH access',
+    ingress=[
+        {'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': ['0.0.0.0/0']},
+    ])
+
+# Create a new EC2 instance
+instance = ec2.Instance('my-instance',
+    instance_type='t2.micro',
+    ami='ami-0905a3c97561e0b69',
+    security_groups=[security_group.name])
+
+pulumi.export('instance_id', instance.id)
+pulumi.export('public_ip', instance.public_ip)
